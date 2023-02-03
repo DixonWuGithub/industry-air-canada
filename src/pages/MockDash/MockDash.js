fccd9460a6ebeac11ae8ec424e9fa0ec72d4f869
Dixon Wu <dxnwuconnect@gmail.com>
2023-02-03 12:13:47 -0800

    add padding to bottom text of modal

--- src/pages/MockDash/MockDash.js
+++ src/pages/MockDash/MockDash.js
@@ -3,22 +3,13 @@ import Header from "../../components/Header/Header"
 import DashboardModal from "../../components/DashboardModal/DashboardModal"
 import content from "../../assets/images/Desktop - 1.png"
 
-function MockDash(){
+function MockDash() {
     return (
         <>
-
-        <DashboardModal /> 
-        <section className="content__holder">
-            <img src={content} alt="" className="content__image" />
-        </section>
-
-        {/* <section className="section">
-            <div className="body-wrap">
-                <div className="mock-dash">
-                This is the mock dashboard page
-                </div>
-            </div>
-        </section> */}
+            <DashboardModal />
+            <section className="content__holder">
+                <img src={content} alt="" className="content__image" />
+            </section>
         </>
     )
 }
