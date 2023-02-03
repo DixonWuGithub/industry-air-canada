2330bd549df1573f8cf350f9385aab4445aa88f6
Dixon Wu <dxnwuconnect@gmail.com>
2023-02-03 09:50:50 -0800

    add modal styling and content as well as backgrount

--- src/pages/MockDash/MockDash.js
+++ src/pages/MockDash/MockDash.js
@@ -1,20 +1,24 @@
 import "./MockDash.scss"
 import Header from "../../components/Header/Header"
 import DashboardModal from "../../components/DashboardModal/DashboardModal"
+import content from "../../assets/images/Desktop - 1.png"
 
 function MockDash(){
     return (
         <>
-        {/* conditionally render the modal */}
+
         <DashboardModal /> 
-        <Header />
-        <section className="section">
+        <section className="content__holder">
+            <img src={content} alt="" className="content__image" />
+        </section>
+
+        {/* <section className="section">
             <div className="body-wrap">
                 <div className="mock-dash">
                 This is the mock dashboard page
                 </div>
             </div>
-        </section>
+        </section> */}
         </>
     )
 }
